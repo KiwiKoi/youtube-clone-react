a28cfea6eeb65c5e7c410088efa0b81cd6181b4f
Daniel <danielo.visage@gmail.com>
2022-08-15 18:19:55 +0200

    set up app

---
+++ src/components/header/header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./header.module.scss"
+
+function Header() {
+  return (
+    <header className={styles.header}>
+        <div>
+            DerpTube
+        </div>
+    </header>
+  )
+}
+
+export default Header
